7e410b18437f1a31739d42de306c8830290fc941
Robert <robertdev95@gmail.com>
2022-07-14 20:06:39 -0400

    Parse and store data
    This commit allows the CSV file to be read correctly
    and the data to be passed into a preparedstatement which
    will be used to save the data in the Postgres DB.

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        PSQLDataImport.importCSVFile("C:\\Users\\prato\\Repos\\setup-database-fin\\bike-data\\bike-data-1.csv");
+        String file = "C:\\Users\\prato\\Repos\\setup-database-fin\\bike-data\\bike-data-1.csv";
+        PSQLDataImport.importCSVFile(file);
     }
 }
